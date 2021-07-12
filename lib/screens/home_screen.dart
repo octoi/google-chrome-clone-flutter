b4264e8bd015939a7d586f9379f692c43a49bf0c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-12 12:45:29 +0530

    google logo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_chrome/utils/data.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                googleLogo,
+                width: MediaQuery.of(context).size.width / 2,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
