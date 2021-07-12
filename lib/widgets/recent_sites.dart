555ebd5b9b373b9d4db143667fd4e4722aba4149
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-12 13:24:09 +0530

    image button

--- lib/widgets/recent_sites.dart
+++ lib/widgets/recent_sites.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_chrome/utils/data.dart';
 
 class RecentSites extends StatelessWidget {
   const RecentSites({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ImageButton(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageButton extends StatelessWidget {
+  const ImageButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {},
+      shape: CircleBorder(),
+      fillColor: appSecondaryColor,
+      elevation: 0.0,
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        width: MediaQuery.of(context).size.width / 4 / 2,
+        child: Image.network(youtubeLogo),
+      ),
+    );
   }
 }
