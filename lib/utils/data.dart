ef37c779b9626dd578ee649915d81b718287b114
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-12 12:33:12 +0530

    all icon

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -12,3 +12,15 @@ const String facebookLogo =
 
 const String instagramLogo =
     'https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-0.png';
+
+const String googlePhotosIcon =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Photos_icon_%282020%29.svg/1024px-Google_Photos_icon_%282020%29.svg.png';
+
+const String googleCalendarIcon =
+    'https://i.pinimg.com/originals/6e/09/90/6e099088b3deb805b68d83676af6f067.png';
+
+const String googleDriveIcon =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png';
+
+const String googleTranslateIcon =
+    'https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png';
