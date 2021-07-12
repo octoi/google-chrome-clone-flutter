fcf48f47bea017619488310be887d7530041613f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-12 12:29:18 +0530

    logos

---
+++ lib/utils/data.dart
@@ -0,0 +1,14 @@
+const String googleLogo =
+    'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+
+const String youtubeLogo =
+    'https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png';
+
+const String twitterLogo =
+    'https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png';
+
+const String facebookLogo =
+    'https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512';
+
+const String instagramLogo =
+    'https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-0.png';
