591bb8f750113ae42ee795528c667900e72d1a9b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-12 13:09:06 +0530

    used search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_chrome/utils/data.dart';
+import 'package:google_chrome/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,22 +18,7 @@ class HomeScreen extends StatelessWidget {
                 width: MediaQuery.of(context).size.width / 2,
               ),
               SizedBox(height: 30.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: TextField(
-                  cursorColor: appAccentColor,
-                  decoration: InputDecoration(
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(width: 0, style: BorderStyle.none),
-                    ),
-                    fillColor: appSecondaryColor,
-                    contentPadding: EdgeInsets.all(20.0),
-                    suffixIcon: Icon(Icons.mic),
-                  ),
-                ),
-              ),
+              SearchBar(),
             ],
           ),
         ),
